efa48af68fd744d25f702e456cee1f89e6d09c3e
Schweidzon <dannielsch@gmail.com>
2023-03-24 10:29:47 -0300

    feat: ver comentários de um post

---
+++ src/components/CommentsIcon.jsx
@@ -0,0 +1,53 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { AiOutlineComment } from "react-icons/ai"
+import styled from "styled-components"
+
+export default function CommentIcon({ postId, seeComments, setSeeComments  }) {
+    const [comments, setComments] = useState([])
+    useEffect(() => {
+
+        axios.get(`${process.env.REACT_APP_API_URL}/comments/${postId}`)
+            .then(res => {
+                setComments(res.data)
+
+            })
+    }, [])
+    
+    return (
+        <>
+            {comments.length > 0 ?
+                <Icon  onClick={() => {
+                    setSeeComments(!seeComments)
+                    console.log(seeComments)
+                    
+                    }}>
+                    <AiOutlineComment size={'25px'}  />
+                    {comments.length} comments
+                </Icon>
+                :
+                <div>Oi</div>
+
+            }
+
+
+        </>
+
+
+
+    )
+}
+
+const Icon = styled.div`
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 11px;
+    cursor: pointer;
+    flex-direction: column!important;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+`
+
--- src/components/UserPosts2.jsx
+++ src/components/UserPosts2.jsx
@@ -3,31 +3,35 @@ import { useContext, useEffect, useState } from "react"
 import { Link, useParams } from "react-router-dom"
 import styled from "styled-components"
 import { AuthContext } from "../providers/auth"
-import LikeButton from "./LikeButton"
-import { Oval, RotatingLines } from "react-loader-spinner"
+import { Oval,  } from "react-loader-spinner"
+import PostInfos from "./PostInfos"
 
 
 export default function UserPost({ reload }) {
 
     const { user } = useContext(AuthContext)
 
-
-  
-
     const [posts, setPosts] = useState([])
-    const [userData, setUserData] = useState([])
-    const [loading, setLoading] = useState(true)
   
+
+    const [loading, setLoading] = useState(true)
+
 
     useEffect(() => {
 
         axios.get(`${process.env.REACT_APP_API_URL}/timeline`)
             .then(res => {
-                setPosts(res.data)
+             
+                setPosts(res.data)   
                 setLoading(false)
-                console.log(res.data)
+              
+                
+                
             })
     }, [reload])
+
+    
+
 
 
 
@@ -60,47 +64,9 @@ export default function UserPost({ reload }) {
 
                             timeline
                         </PageTitle>
-                        {
-                            posts.map((post =>
-                                <ContainerPost data-test="post">
-                                    <>
-                                        <ProfilePicDiv>
-                                            <img src={post.image_url} alt='profilepic' />
-                                            <LikeButton postId={post.id} />
-                                        </ProfilePicDiv>
-
-                                        <MainDiv  >
-                                            <HeaderPost>
-                                                <div>
-                                                    <Link to={"/users/28"} data-test="username" >{post.user_name}</Link>
-                                                    <h3 data-test="description" >{post.description} {post.hashtags.map((h) => <StyledLink to={`/hashtag/${h.text}`}>{` #${h.text}  `}</StyledLink>)}</h3>
-                                                </div>
-                                                {post.user_id === user.user.id &&
-                                                    <ChangeButton>
-                                                        <ion-icon name="pencil"></ion-icon>
-                                                        <ion-icon name="trash"></ion-icon>
-                                                    </ChangeButton>
-
-                                                } 
-
-                                            </HeaderPost>
-                                            <PostContent data-test="link" onClick={() => window.open(`${post.link}`, "_blank")}>
-                                                <div>
-                                                    <h1>{post.title}</h1>
-                                                    <h2>{post.postDescription}</h2>
-                                                    <a href={post.link} target="_blank">{post.link}</a>
-                                                </div>
-                                                <img src={post.image} />
-                                            </PostContent>
-                                        </MainDiv>
-                                    </>
-
-                                </ContainerPost>
-                            ))
-                        }
-
-
-
+                        {posts.map((p) => <PostInfos post={p} />)}
+                        
+                       
 
                     </>
                     :
@@ -162,147 +128,10 @@ const PageTitle = styled.div`
             border-radius: 26.5px;
     }
 
-            `
+`
 
-const ContainerPost = styled.div`
-            display: flex;
-            flex-direction: row;
-            width: 611px;
-            /* height: 278px; */
-            background: #171717;
-            margin: 30px;
-            border-radius: 16px;
+  
 
 
-            p{
-                color: #000000;
-    }
-            `
 
 
-const HeaderPost = styled.div`
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            width: 500px;
-            height: 30px;
-            color: white;
-            margin-top: 20px;
-            font-family: Lato;
-            font-size: 19px;
-            font-weight: 400;
-            line-height: 23px;
-            letter-spacing: 0em;
-            text-align: left;
-
-            h2{
-                padding-left: 3px;
-    }
-            div{
-                display: flex;
-            flex-direction: column;
-            gap: 10px;
-            margin-top: 10px;
-
-            ion-icon{
-                font-size: 22px;
-            margin: 0px 5px;
-        }
-    }
-            h3 {
-                font-family: 'Lato';
-            font-style: normal;
-            font-weight: 400;
-            font-size: 17px;
-            color: #B7B7B7
-    }
-            `
-const PostContent = styled.div`
-            display: flex;
-            width: 503px;
-            border: 1px solid #4D4D4D;
-            border-radius: 11px;
-            margin-top: 40px;
-            cursor: pointer;
-            word-wrap: break-word!important;
-            div {
-                font-family: 'Lato';
-            font-style: normal;
-            font-weight: 400;
-            padding: 15px;
-            word-wrap: break-word!important;
-            display: flex;
-            flex-direction: column;
-            gap: 10px;
-
-
-            h1 {
-                font-size: 16px;
-            color: #CECECE
-        }
-            h2 {
-                font-size: 11px;
-            color: #9B9595;     
-           
-        }
-            a {
-                font-size: 11px;
-            color: #CECECE;
-            
-        }
-    }
-
-            img {
-                width: 153.44px;
-            height: inherit;
-            border-radius: 0px 12px 13px 0px;
-    }
-
-            p{
-                width: 500px;
-            font-family: Lato;
-            font-size: 17px;
-            font-weight: 400;
-            line-height: 20px;
-            text-align: left;
-            color: #FFFFFF;
-            margin: 10px 0px;
-            padding-left: 3px;
-    }
-            `
-const ProfilePicDiv = styled.div`
-            display: flex;
-            align-items: center;
-            width: 96px;
-            flex-direction: column;
-
-            img{
-                width: 50px;
-            border-radius: 50%;
-            margin: 18px;
-    }
-
-            ion-icon{
-                font-size: 30px;
-    }
-            p{
-                font-family: Lato;
-            font-size: 11px;
-            font-weight: 400;
-            text-align: center;
-            color: #FFFFFF;
-    }
-            `
-const MainDiv = styled.div`
-            display: flex;
-            flex-direction: column;
-            width: 611px;
-            height: 276px;
-            background: #171717;
-            border-radius: 16px;
-            `
-
-const ChangeButton = styled.div`
-            flex-direction:row!important;
-
-            `
